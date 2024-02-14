2cac39b01f39500ce11ba43133f925900a61ecbc
albertoferri <falberto99@gmail.com>
2024-02-14 17:43:26 +0100

    refactor codice, e cambio ciclo for in un ciclo foreach

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
-/*
-Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal. 
-Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
-Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
-*/
+
+// Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal. 
+// Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+// Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+
 
 
 // bersagliamo lo slider
 // tramite un ciclo for prendiamo ogni indirizzo delle immagini dall'array
 // per ognuno di essi andremo a creare un elemento img dentro lo slider
 
-const images = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"];
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
 console.log(images);
 
 
@@ -18,30 +40,30 @@ const sliderElement = document.getElementById("slider");
 
 
 // tramite un ciclo for prendiamo ogni indirizzo delle immagini dall'array
-for (let i = 0; i < images.length; i++) {
+images.forEach((slider, i) => {
 
     sliderElement.innerHTML += `<img src="./img/0${i + 1}.webp" alt="immagine ${i + 1}">`;
 
-    // per ognuno di essi andremo a creare un elemento img dentro lo slider
 
-}
+    console.log(slider)
+});
 
 document.querySelector("#slider img:nth-of-type(1)").className = "active";
 
-/*
--  salvo un contatore della slide
--  QUANDO premo la freccia SU
-    - prendo l'immagine attuale e le rimuovo la classe "active"  
-    - aumento il contatore di 1
-    - prendo l'immagine con il nuovo contatore e le aggiungo la classe "active"
-*/
+
+// -  salvo un contatore della slide
+// -  QUANDO premo la freccia SU
+//     - prendo l'immagine attuale e le rimuovo la classe "active"  
+//     - aumento il contatore di 1
+//     - prendo l'immagine con il nuovo contatore e le aggiungo la classe "active"
+
 
 
 // -  salvo un contatore della slide
 let slideNumber = 1;
 
 // -  QUANDO premo la freccia SU
-document.querySelector("#up-arrow").addEventListener("click", function() {
+document.querySelector("#right-arrow").addEventListener("click", function() {
 
 
     if (slideNumber < images.length) {
@@ -74,7 +96,7 @@ document.querySelector("#up-arrow").addEventListener("click", function() {
 });
 
 
-document.querySelector("#down-arrow").addEventListener("click", function() {
+document.querySelector("#left-arrow").addEventListener("click", function() {
 
     if (slideNumber > 1) {
         // - prendo l'immagine attuale e le rimuovo la classe "active"  
